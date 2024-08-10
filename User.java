d071add411eb55c5fe09ac18d89e348a8dd34274
Prasad <147171100+prasadmagdum@users.noreply.github.com>
2024-08-10 20:15:46 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,17 @@
+public class User {
+    public int regId;
+    public String name;
+    public int age;
+    public String email;
+    public String username;
+    public String password;
+
+    public User(int regId, String name, int age, String email, String username, String password) {
+        this.regId = regId;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+}
